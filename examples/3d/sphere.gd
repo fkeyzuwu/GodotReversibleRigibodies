af9fb90bede27d6a2fe8e85be38b846546b157a9
fkeyz <fkeyzuwu@gmail.com>
2023-07-24 21:27:31 +0300

    added outline material to 3d example

--- examples/3d/sphere.gd
+++ examples/3d/sphere.gd
@@ -1,8 +1,12 @@
 extends ReversibleRigidBody3D
 
+@onready var mesh_instance: MeshInstance3D = $MeshInstance3D
+
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
 		if event.pressed and event.keycode == KEY_R:
 			start_rewind()
+			mesh_instance.mesh.material.next_pass.set_shader_parameter("enable", true)
 		elif !event.pressed and event.keycode == KEY_R:
 			stop_rewind()
+			mesh_instance.mesh.material.next_pass.set_shader_parameter("enable", false)
