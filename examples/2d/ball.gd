b47f32ac28b0bd0eb2c12fcc6370aaf9ba7cf069
fkeyz <fkeyzuwu@gmail.com>
2023-07-24 22:39:46 +0300

    added so you cant spam spacebar

--- examples/2d/ball.gd
+++ examples/2d/ball.gd
@@ -4,10 +4,11 @@ var force := 500.0
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
-		if event.pressed and event.keycode == KEY_SPACE and !rewinding:
-			var direction = Vector2(randf_range(-1 , 1), randf_range(-1 , 1))
-			apply_central_impulse(direction * force)
-		elif event.pressed and event.keycode == KEY_R:
-			start_rewind()
-		elif !event.pressed and event.keycode == KEY_R:
-			stop_rewind()
+		if !event.echo:
+			if event.pressed and event.keycode == KEY_SPACE and !rewinding:
+				var direction = Vector2(randf_range(-1 , 1), randf_range(-1 , 1))
+				apply_central_impulse(direction * force)
+			elif event.pressed and event.keycode == KEY_R:
+				start_rewind()
+			elif !event.pressed and event.keycode == KEY_R:
+				stop_rewind()
--- examples/3d/sphere.gd
+++ examples/3d/sphere.gd
@@ -6,12 +6,13 @@ var force := 5
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
-		if event.pressed and event.keycode == KEY_SPACE and !rewinding:
-			var direction = Vector3(randf_range(-1 , 1), randf_range(0 , 1), randf_range(-1 , 1))
-			apply_central_impulse(direction * force)
-		if event.pressed and event.keycode == KEY_R:
-			start_rewind()
-			mesh_instance.mesh.material.next_pass.set_shader_parameter("enable", true)
-		elif !event.pressed and event.keycode == KEY_R:
-			stop_rewind()
-			mesh_instance.mesh.material.next_pass.set_shader_parameter("enable", false)
+		if !event.echo: #just pressed
+			if event.pressed and event.keycode == KEY_SPACE and !rewinding:
+				var direction = Vector3(randf_range(-1 , 1), randf_range(0 , 1), randf_range(-1 , 1))
+				apply_central_impulse(direction * force)
+			if event.pressed and event.keycode == KEY_R:
+				start_rewind()
+				mesh_instance.mesh.material.next_pass.set_shader_parameter("enable", true)
+			elif !event.pressed and event.keycode == KEY_R:
+				stop_rewind()
+				mesh_instance.mesh.material.next_pass.set_shader_parameter("enable", false)
