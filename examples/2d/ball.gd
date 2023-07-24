c33d7efd9fcbc2d08b2d942cef15b0f204d179f9
fkeyz <fkeyzuwu@gmail.com>
2023-07-24 22:13:00 +0300

    sort of works, needs to be better tho.

--- examples/2d/ball.gd
+++ examples/2d/ball.gd
@@ -4,7 +4,7 @@ var force := 500.0
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
-		if event.pressed and event.keycode == KEY_SPACE:
+		if event.pressed and event.keycode == KEY_SPACE and !rewinding:
 			var direction = Vector2(randf_range(-1 , 1), randf_range(-1 , 1))
 			apply_central_impulse(direction * force)
 		elif event.pressed and event.keycode == KEY_R:
